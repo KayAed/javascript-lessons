f6734f3dd910a21d9efcb6a3c4a4e266f9409edf
Khadijat Abdullahi <khaptainkhad@gmail.com>
2025-09-30 12:37:22 +0100

    Adjust MDN's code to allow calendar show correct number of days per month using if..else if..

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 //js for theme changer.
+/*
 const select = document.querySelector("select");
 const html = document.querySelector("html");
 document.body.style.padding = "10px";
@@ -12,7 +13,44 @@ select.addEventListener("change", () =>
 select.value === "black"
 ? update("black", "white")
 : update("white", "black"),
-);
+);  */
+
+//Here, we create a calendar
+const select = document.querySelector("select");
+const list = document.querySelector("ul");
+const h1 = document.querySelector("h1");
+
+select.addEventListener("change", () => {
+  const choice = select.value;
+  createCalendar(choice);
+});
+
+function createCalendar(month) {
+  let days = 31;
+
+  // ADD CONDITIONAL HERE
+  if (month === "January" || month === "March" || month === "May" || month === "July" 
+    || month === "July" || month === "October" || month === "December") {
+      days = 31;
+    } else if (month === "May" || month === "June" || month === "September" || month === "November") {
+      days = 30;
+    }
+    else {
+      days = 28;
+  }
+
+  list.textContent = "";
+  h1.textContent = month;
+  for (let i = 1; i <= days; i++) {
+    const listItem = document.createElement("li");
+    listItem.textContent = i;
+    list.appendChild(listItem);
+  }
+}
+
+select.value = "January";
+createCalendar("January");
+
 
 // Function: creates a new paragraph and appends it to the bottom of the HTML body.
 function createParagraph() {
