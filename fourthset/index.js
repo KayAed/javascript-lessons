38eef5be0172d2b542656f07835c78ba09dd399f
Khadijat Abdullahi <khaptainkhad@gmail.com>
2025-11-08 08:48:18 +0100

    Loop to countdown and another to create two separte lists from one.

--- fourthset/index.js
+++ fourthset/index.js
@@ -17,3 +17,32 @@ while (i >= 0) {
 
   i--;
 }
+
+const people = [
+  "Chris",
+  "Anne",
+  "Colin",
+  "Terri",
+  "Phil",
+  "Lola",
+  "Sam",
+  "Kay",
+  "Bruce",
+];
+
+const admitted = document.querySelector(".admitted");
+const refused = document.querySelector(".refused");
+admitted.textContent = "Admit: ";
+refused.textContent = "Refuse: ";
+
+// loop starts here
+for (const persons of people) {
+  if (persons === "Lola" || persons === "Phil") {
+    refused.textContent += `${persons}, `;
+  } else {
+    admitted.textContent += `${persons}, `;
+  }
+}
+
+refused.textContent = `${refused.textContent.slice(0, -2)}.`;
+admitted.textContent = `${admitted.textContent.slice(0, -2)}.`;
